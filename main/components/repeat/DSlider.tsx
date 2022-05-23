d7b29323eba3d49f63c454291f288048b27ece4d
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-05-23 13:27:03 +0400

    FIX: issue causing scrolling on button click

--- main/components/repeat/DSlider.tsx
+++ main/components/repeat/DSlider.tsx
@@ -1,58 +1,59 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Autoplay, EffectFade, Navigation, A11y } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay, EffectFade, Navigation, A11y } from 'swiper';
 import { styled } from '@mui/system';
 import Typography from '@mui/material/Typography';
 import DCard from './DCard';
 import { IconButton } from '@mui/material';
 
 const DSwiper = styled(Swiper)(({ theme }) => ({
-    padding: "20px",
-    "--swiper-navigation-size": "10px",
+    padding: '20px',
+    '--swiper-navigation-size': '10px',
     '& .swiper-button-prev': {
-        top: "0px"
+        top: '0px',
     },
     '& .swiper-button-next': {
-        top: "0px"
-    }
+        top: '0px',
+    },
 }));
 
 const DSlider = ({ title, itemData }: any) => {
     return (
         <React.Fragment>
             {itemData && Object.keys(itemData).length !== 0 ? (
-                <div style={{padding: "20px"}}>
+                <div style={{ padding: '20px' }}>
                     <Typography
-                        sx={{ padding: "0px 20px", display: "flex", alignItems: "center" }}
+                        sx={{ padding: '0px 20px', display: 'flex', alignItems: 'center' }}
                         variant="h5"
                     >
                         {title || null}
                     </Typography>
                     <IconButton></IconButton>
-                                        <DSwiper
-                                        
+                    <DSwiper
+                        grabCursor={true}
+                        watchSlidesProgress={true}
                         breakpoints={{
-                            "240": {
+                            '240': {
                                 slidesPerView: 2,
                                 spaceBetween: 10,
                             },
-                            "385": {
+                            '385': {
                                 slidesPerView: 2,
                                 spaceBetween: 10,
                             },
-                            "570": {
+                            '570': {
                                 slidesPerView: 4,
                                 spaceBetween: 10,
                             },
-                            "760": {
+                            '760': {
                                 slidesPerView: 5,
                                 spaceBetween: 10,
                             },
-                            "940": {
+                            '940': {
                                 slidesPerView: 6,
                                 spaceBetween: 20,
                             },
-                            "1194": {
+                            '1194': {
                                 slidesPerView: 7,
                                 spaceBetween: 20,
                             },
@@ -65,11 +66,9 @@ const DSlider = ({ title, itemData }: any) => {
                         ))}
                     </DSwiper>
                 </div>
-            ) : (
-                null
-            )}
+            ) : null}
         </React.Fragment>
-    )
-}
+    );
+};
 
-export default DSlider
+export default DSlider;
