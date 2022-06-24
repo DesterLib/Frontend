d7801eee8bf3ab572a69fff22e275ac5d5fe001b
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-06-25 00:20:13 +0400

    REFACTOR: mpv

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,55 @@
 const { app, BrowserWindow, protocol } = require('electron');
 const path = require('path');
 const url = require('url');
-const { getPluginEntry } = require('mpv.js');
+const process = require('process');
+
+/**
+ * The MIME type associated with mpv.js plugin.
+ */
+ const PLUGIN_MIME_TYPE = 'application/x-mpvjs';
+
+ function containsNonASCII(str) {
+     for (let i = 0; i < str.length; i++) {
+         if (str.charCodeAt(i) > 255) {
+             return true;
+         }
+     }
+     return false;
+ }
+ 
+ /**
+  * Return value to be passed to `register-pepper-plugins` switch.
+  *
+  * @param {string} pluginDir - Plugin directory
+  * @param {string} [pluginName=mpvjs.node] - Plugin name
+  * @throws {Error} Resulting path contains non-ASCII characters.
+  */
+ function getPluginEntry(pluginDir, pluginName = 'mpvjs.node') {
+     const fullPluginPath = path.join(pluginDir, pluginName);
+     // Try relative path to workaround ASCII-only path restriction.
+     let pluginPath = path.relative(process.cwd(), fullPluginPath);
+     if (path.dirname(pluginPath) === '.') {
+         // "./plugin" is required only on Linux.
+         if (process.platform === 'linux') {
+             pluginPath = `.${path.sep}${pluginPath}`;
+         }
+     } else {
+         // Relative plugin paths doesn't work reliably on Windows, see
+         // <https://github.com/Kagami/mpv.js/issues/9>.
+         if (process.platform === 'win32') {
+             pluginPath = fullPluginPath;
+         }
+     }
+     if (containsNonASCII(pluginPath)) {
+         if (containsNonASCII(fullPluginPath)) {
+             throw new Error('Non-ASCII plugin path is not supported');
+         } else {
+             pluginPath = fullPluginPath;
+         }
+     }
+     return `${pluginPath};${PLUGIN_MIME_TYPE}`;
+ }
+ 
 
 let os;
 switch (process.platform) {
@@ -13,7 +61,7 @@ switch (process.platform) {
         break;
 }
 
-const pdir = path.join(__dirname, '..', 'node_modules', '@desterlib', 'dplayer', 'mpv', os);
+const pdir = path.join(__dirname, '..', 'node_modules', '@desterlib', 'dplayer', 'dist');
 
 if (process.platform !== 'linux') {
     process.chdir(pdir);
--- src/decleration.d.ts
+++ src/decleration.d.ts
@@ -4,5 +4,3 @@ declare module '*.jpg';
 declare module '*.png';
 declare module '*.svg';
 declare module 'react-color';
-declare module 'mpv.js';
-declare module 'dester-dplayer';
